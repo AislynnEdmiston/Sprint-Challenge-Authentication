02d889f50e90546d62ec5f41d542ee4cf7167468
AislynnEdmiston <davidandaislynn@gmail.com>
2020-03-21 14:31:40 -0400

    started on register

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs")
+const db = require("../database/dbConfig")
 
-router.post('/register', (req, res) => {
-  // implement registration
+// const Users = require("./user-model")
+
+router.post('/register', async (req, res, next) => {
+    try {
+        const { username } = req.body
+        const password = req.body.password
+
+        console.log({username})
+        const user = await db("users").select("id", "username", "password").where({ username })
+        const hash = await bcrypt.hash(password, 13)
+        console.log(req.body)
+        if (user === { username }) {
+            return res.status(409).json({
+                message: "The username is already taken please try again"
+            })
+        }
+
+        res.status(201).json(await db("users").insert(user))
+    } catch (err) {
+        console.log(err)
+        next(err)
+    }
 });
 
 router.post('/login', (req, res) => {
