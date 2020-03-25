9b5e3944fa085fa547ba0014cd995e76b4b32056
AislynnEdmiston <davidandaislynn@gmail.com>
2020-03-25 17:48:38 -0400

    created my middleware to check if user is loggedin

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,33 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require("jsonwebtoken")
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+function restrict() {
+  const authErr = {
+    message: "invalid credentials",
+  }
+
+  return async (req, res, next) => {
+    try {
+      const { token } = req.cookies
+
+      if(!token) {
+        return res.status(401).json(authErr)
+      }
+
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).json(authErr)
+        }
+
+        req.token = decoded
+
+        next()
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = restrict
+
+
